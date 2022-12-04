63c83d52e925108536a82730e432e0c828ca758c
wh015n3w <akunbaruwisnu@gmail.com>
2022-12-04 14:34:48 +0700

    [completion] Assigment 2 FSJS-5

--- assigment-2/tailwind.config.js
+++ assigment-2/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        background: "url('./src/assets/background.jpg') #030A15",
+        parallax: "url('./src/assets/talitha.jpg')",
+        porto1: "url('./src/assets/kirana.jpg')",
+        porto2: "url('./src/assets/lila.jpg')",
+        porto3: "url('./src/assets/talpav.jpg')",
+        porto4: "url('./src/assets/shanika.jpg')",
+        shape: "url('./src/assets/Shape.png')",
+        testi: "url('./src/assets/Vector.jpg')",
+        footer: "url('./src/assets/bg.png')",
       },
       colors: {
         primary: "#FE8C12",
